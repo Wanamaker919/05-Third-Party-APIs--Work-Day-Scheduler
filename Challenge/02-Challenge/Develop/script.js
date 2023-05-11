9f647e6713c1a97ff447a57969df88cf2a26619f
Wanamaker919 <116662904+Wanamaker919@users.noreply.github.com>
2023-05-10 21:02:21 -0400

    task day working

--- Challenge/02-Challenge/Develop/script.js
+++ Challenge/02-Challenge/Develop/script.js
@@ -21,3 +21,66 @@ $(function () {
   //
   // TODO: Add code to display the current date in the header of the page.
 });
+$(function () {
+  // Add click listener for save buttons
+  $(".saveBtn").on("click", function() {
+    // Get the user input from the textarea
+    var description = $(this).siblings(".description").val();
+    // Get the id of the containing time-block
+    var id = $(this).parent().attr("id");
+    // Save the user input in local storage using the id as a key
+    localStorage.setItem(id, description);
+  });
+
+  // Apply past, present, or future classes to time-blocks
+  function updateHourClasses() {
+    // Get the current hour using Day.js
+    var currentHour = dayjs().hour();
+
+    // Loop through each time-block
+    $(".time-block").each(function() {
+      // Get the hour of the time-block using its id
+      var blockHour = parseInt($(this).attr("id").split("-")[1]);
+
+      // Remove any existing past, present, or future classes
+      $(this).removeClass("past present future");
+
+      // Add the appropriate class based on the current hour and block hour
+      if (blockHour < currentHour) {
+        $(this).addClass("past");
+      } else if (blockHour === currentHour) {
+        $(this).addClass("present");
+      } else {
+        $(this).addClass("future");
+      }
+    });
+  }
+
+  // Get any saved user input and update the corresponding textarea elements
+  function updateDescriptions() {
+    // Loop through each time-block
+    $(".time-block").each(function() {
+      // Get the id of the time-block
+      var id = $(this).attr("id");
+
+      // Get the saved user input from local storage
+      var description = localStorage.getItem(id);
+
+      // Update the textarea element with the saved user input
+      $(this).find(".description").val(description);
+    });
+  }
+
+  // Display the current date in the header of the page
+  $("#currentDay").text(dayjs().format("dddd, MMMM D"));
+
+  // Call the functions to apply classes and update descriptions
+  updateHourClasses();
+  updateDescriptions();
+
+  // Set an interval to update the hour classes and descriptions every minute
+  setInterval(function() {
+    updateHourClasses();
+    updateDescriptions();
+  }, 60000);
+});
